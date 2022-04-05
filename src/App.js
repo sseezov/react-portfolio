ff7c5ab1ef3ab568069c73f77d35d4f167a747a6
sseezov <sseezov@mail.ru>
2022-04-05 20:17:24 +0700

    letters animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.scss'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/layout/layout'
+import Home from './components/home/home'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </>
   )
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import Sidebar from '../sidebar/sidebar'
 import './layout.scss'
 
@@ -5,6 +6,19 @@ const Layout = () => {
   return (
     <div>
       <Sidebar />
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+
+        <span className="tags bottom-tags">
+          &lt;body&gt;
+          <br />
+          <span span className="bottom-tag-html">
+            &lt;/html&gt;
+          </span>
+        </span>
+      </div>
     </div>
   )
 }
