396a16c67f4f25b9b3e5a8b9e93d9b393f62b7e9
sseezov <sseezov@mail.ru>
2022-04-06 15:12:08 +0700

    break spot

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.scss'
 import { Route, Routes } from 'react-router-dom'
-import Layout from './components/layout/layout'
-import Home from './components/home/home'
-import About from './components/about/about'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Layout from './components/Layout'
+import './App.scss'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react'
-import Loader from 'react-loaders'
 import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
 import LogoTitle from '../../assets/images/logo-s.png'
-import AnimatedLetters from '../animatedLetters/animatedLetters'
-import './home.scss'
-import Logo from './logo/logo'
+import Logo from './Logo'
+import './index.scss'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const nameArray = ['s', 'e', 'e', 'z', 'o', 'v']
+
+  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
   const jobArray = [
     'w',
     'e',
@@ -23,6 +24,7 @@ const Home = () => {
     'p',
     'e',
     'r',
+    '.',
   ]
 
   useEffect(() => {
@@ -39,9 +41,12 @@ const Home = () => {
             <span className={letterClass}>H</span>
             <span className={`${letterClass} _12`}>i,</span>
             <br />
-            <span className={`${letterClass} _13`}>I`</span>
-            <span className={`${letterClass} _14`}>m</span>
-            <img src={LogoTitle} alt="hi" />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img
+              src={LogoTitle}
+              alt="JavaScript Developer Name, Web Developer Name"
+            />
             <AnimatedLetters
               letterClass={letterClass}
               strArray={nameArray}
@@ -51,17 +56,17 @@ const Home = () => {
             <AnimatedLetters
               letterClass={letterClass}
               strArray={jobArray}
-              idx={21}
+              idx={22}
             />
           </h1>
-          <h2>
-            <Link to="/contact" className="flat-button">
-              CONTACT ME
-            </Link>
-          </h2>
+          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
         </div>
         <Logo />
       </div>
+
       <Loader type="pacman" />
     </>
   )
